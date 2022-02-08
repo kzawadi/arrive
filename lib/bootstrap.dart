724adc0f9aac71b5d2ee565d11a1e661512960db
kelvin <kelvin.vitone@gmail.com>
2022-02-08 03:10:13 +0300

    added a native splash screen for android and ios

---
+++ lib/bootstrap.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+import 'dart:developer';
+
+import 'package:at_utils/at_logger.dart';
+import 'package:atsign_location_app/injections.dart';
+import 'package:bloc/bloc.dart';
+import 'package:bloc_concurrency/bloc_concurrency.dart';
+import 'package:flutter/widgets.dart';
+import 'package:injectable/injectable.dart';
+
+//This is a entry with all the necesary app level initializations
+//things like ensuring initializaion and BloC Observer plus the
+//[AtSignLogger].
+class AppBlocObserver extends BlocObserver {
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    log('onChange(${bloc.runtimeType}, $change)');
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    log('onError(${bloc.runtimeType}, $error, $stackTrace)');
+    super.onError(bloc, error, stackTrace);
+  }
+}
+
+Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
+  FlutterError.onError = (details) {
+    log(details.exceptionAsString(), stackTrace: details.stack);
+  };
+  await runZonedGuarded(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
+      configureInjection(Environment.prod);
+      AtSignLogger.root_level = 'finer';
+
+      await BlocOverrides.runZoned(
+        () async => runApp(await builder()),
+        blocObserver: AppBlocObserver(),
+        eventTransformer: sequential<dynamic>(),
+      );
+    },
+    (error, stackTrace) => log(error.toString(), stackTrace: stackTrace),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,6 @@
-import 'dart:async';
-
-import 'package:at_utils/at_logger.dart';
 import 'package:atsign_location_app/app.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:atsign_location_app/bootstrap.dart';
 
 void main() {
-  AtSignLogger.root_level = 'finer';
-  WidgetsFlutterBinding.ensureInitialized();
-  runZoned<Future<void>>(() async {
-    await SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-    ]).then((_) {
-      runApp(const MyApp());
-    });
-  });
+  bootstrap(() => const MyApp());
 }
